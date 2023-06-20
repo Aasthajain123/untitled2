1de87418f9602896e0b9d79265c5fc05b8f74d82
Aastha Jain <btbtc18095_aastha@banasthali.in>
2023-06-20 14:20:58 +0530

    Checking

---
+++ src/ecommerce/Product.java
@@ -0,0 +1,14 @@
+package ecommerce;
+
+public class Product {
+    private final int x;
+
+    public Product(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+}
---
+++ src/ecommerce/ecommerceMain.java
@@ -0,0 +1,8 @@
+package ecommerce;
+
+public class ecommerceMain {
+    public static void main(String[] args) {
+        Product product = new Product(5);
+        System.out.println(product.getX());
+    }
+}
